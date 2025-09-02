a4d9ebb50fbe1656fac05f4dfd27d5f972510638
Praveenkarmegam <lovelypraveendp@gmail.com>
2025-09-02 20:22:25 +0530

    updated

--- gmailClone-Frontend/src/services/emailService.js
+++ gmailClone-Frontend/src/services/emailService.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
-//local
-
-// export const API_BASE = 'http://localhost:5000/api/emails';
+// local
+// export const API_BASE = 'http://localhost:5000/api';
 
 // render
-
 export const API_BASE = 'https://gmail-clone-1-qw9m.onrender.com/api';
-
 
 // Create axios instance
 const axiosInstance = axios.create({
@@ -25,67 +22,67 @@ axiosInstance.interceptors.request.use((config) => {
 
 // Folder Fetching
 export const getInboxEmails = async () => {
-  const res = await axiosInstance.get('/inbox');
+  const res = await axiosInstance.get('/emails/inbox');
   return res.data.data;
 };
 
 export const getStarredEmails = async () => {
-  const res = await axiosInstance.get('/starred');
+  const res = await axiosInstance.get('/emails/starred');
   return res.data.data;
 };
 
 export const getSentEmails = async () => {
-  const res = await axiosInstance.get('/sent');
+  const res = await axiosInstance.get('/emails/sent');
   return res.data.data;
 };
 
 export const getDraftEmails = async () => {
-  const res = await axiosInstance.get('/drafts');
+  const res = await axiosInstance.get('/emails/drafts');
   return res.data.data;
 };
 
 export const getSnoozedEmails = async () => {
-  const res = await axiosInstance.get('/snoozed');
+  const res = await axiosInstance.get('/emails/snoozed');
   return res.data.data;
 };
 
 export const getTrashEmails = async () => {
-  const res = await axiosInstance.get('/trash');
+  const res = await axiosInstance.get('/emails/trash');
   return res.data.data;
 };
 
 // Mail By ID
 export const getEmailById = async (id) => {
-  const res = await axiosInstance.get(`/item/${id}`);
+  const res = await axiosInstance.get(`/emails/item/${id}`);
   return res.data.data;
 };
 
 // Compose
 export const sendEmail = (formData) =>
-  axiosInstance.post('/send', formData, {
+  axiosInstance.post('/emails/send', formData, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
 
 // Draft
 export const saveDraft = (formData) =>
-  axiosInstance.post('/draft', formData, {
+  axiosInstance.post('/emails/draft', formData, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
 
 // Star Toggle
-export const toggleStar = (id) => axiosInstance.put(`/${id}/star`);
+export const toggleStar = (id) => axiosInstance.put(`/emails/${id}/star`);
 
 // Mark as Read
-export const markAsRead = (id) => axiosInstance.put(`/${id}/read`);
+export const markAsRead = (id) => axiosInstance.put(`/emails/${id}/read`);
 
 // Snooze
-export const snoozeEmail = (id) => axiosInstance.patch(`/${id}/snooze`);
+export const snoozeEmail = (id) => axiosInstance.patch(`/emails/${id}/snooze`);
 
 // Move to Trash
-export const deleteEmailById = (id) => axiosInstance.delete(`/${id}`);
+export const deleteEmailById = (id) => axiosInstance.delete(`/emails/${id}`);
 
 // Restore
-export const restoreFromTrash = (id) => axiosInstance.patch(`/restore/${id}`);
+export const restoreFromTrash = (id) => axiosInstance.patch(`/emails/restore/${id}`);
 
 // Delete Forever
-export const deleteForever = (id) => axiosInstance.delete(`/delete-forever/${id}`);
+export const deleteForever = (id) => axiosInstance.delete(`/emails/delete-forever/${id}`);
