eca9752ddaf8d97ea6c9b396102ea95d4274a960
Praveenkarmegam <lovelypraveendp@gmail.com>
2025-09-02 19:58:42 +0530

    updated

--- gmailClone-Backend/server.js
+++ gmailClone-Backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(cors({
   // origin: 'http://localhost:5173', 
-  origin: 'https://cozy-pavlova-5f2ea7.netlify.app/',
+  origin: 'https://cozy-pavlova-5f2ea7.netlify.app',
   credentials: true,
 }));
 app.use(express.json());
